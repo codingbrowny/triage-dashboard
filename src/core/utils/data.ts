c727e324dbf1280c7e0ccdeaa46ce05782432cc4
codingbrowny <codingbrowny@gmail.com>
2023-03-13 15:44:03 -0700

    Static data for app

---
+++ src/core/utils/data.ts
@@ -0,0 +1,344 @@
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import Person3OutlinedIcon from '@mui/icons-material/Person3Outlined';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+export const SidebarLinks = [
+  {
+    name: "Dashboard",
+    path: "/",
+    icon: DashboardOutlinedIcon,
+  },
+  {
+    name: "Doctors",
+    path: "/doctors",
+    icon: Person3OutlinedIcon,
+  },
+  {
+    name: "Cases",
+    path: "/cases",
+    icon: AssignmentOutlinedIcon,
+  },
+];
+
+/**Chart Labels (Months) */
+export const ChartLabels = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const DoctorsData = {
+  Column: [
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 230
+    },
+    {
+      field: "id",
+      headerName: "ID",
+      maxWidth: 150,
+      minWidth: 120
+    },
+    {
+      field: "speciality",
+      headerName: "Speciality",
+      minWidth: 150
+    },
+    {
+      field: "subscription",
+      headerName: "Subscription Period",
+      minWidth: 200
+    },
+    {
+      field: "action",
+      headerName: "",
+      maxWidth: 100,
+      minWidth: 100
+    },
+  ],
+  Data: [
+    {
+      id: 14365870,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "90 days",
+    },
+    {
+      id: 11436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "60 days",
+    },
+    {
+      id: 21436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "18 days",
+    },
+    {
+      id: 31436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "90 days",
+    },
+    {
+      id: 41436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "6 days",
+    },
+    {
+      id: 51436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "20 days",
+    },
+    {
+      id: 61436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "6 days",
+    },
+    {
+      id: 71436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "no subscription",
+    },
+    {
+      id: 81436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "50 days",
+    },
+    {
+      id: 91436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription: "19 days",
+    },
+  ],
+};
+export const ConsultantsData = {
+  Column: [
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 230
+    },
+    {
+      field: "id",
+      headerName: "ID",
+      maxWidth: 150,
+      minWidth: 120
+    },
+    {
+      field: "speciality",
+      headerName: "Speciality",
+      minWidth: 200
+    },
+    {
+      field: "subscription_type",
+      headerName: "Subscription Period",
+      minWidth: 200
+    },
+    {
+      field: "action",
+      headerName: "",
+      maxWidth: 100,
+      minWidth: 100
+    },
+  ],
+  Data: [
+    {
+      id: 14365870,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Emergency Medicine",
+      subscription_type: "consultant",
+    },
+    {
+      id: 11436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Orthopedics",
+      subscription_type: "consultant",
+    },
+    {
+      id: 21436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Obs and Gyanea",
+      subscription_type: "consultant",
+    },
+    {
+      id: 31436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "ENT",
+      subscription_type: "consultant",
+    },
+    {
+      id: 41436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription_type: "consultant",
+    },
+    {
+      id: 51436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Opthalmology",
+      subscription_type: "consultant",
+    },
+    {
+      id: 61436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "OBS and Gyanea",
+      subscription_type: "consultant",
+    },
+    {
+      id: 71436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription_type: "consultant",
+    },
+    {
+      id: 81436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "ENT",
+      subscription_type: "consultant",
+    },
+    {
+      id: 91436587,
+      name: "Dr. Emmanuel Ashley",
+      speciality: "Surgery",
+      subscription_type: "consultant",
+    },
+  ],
+};
+
+export const CasesData = {
+    Column: [
+        {field: "id", headerName: "ID"},
+        {field: "description", headerName: "Description"},
+        {field: "comments", headerName: "No. of Comments"},
+        {field: "status", headerName: "Status"},
+        {field: "action", headerName: ""},
+    ],
+    Data: [
+        {
+            id: 11345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 12345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 92345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 82345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 12345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 72345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 62345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 52345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 42345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 32345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+        {
+            id: 22345678,
+            description: "A 23 year old boy has been diagnosed with malaria but he's unable to recover",
+            comments: 12,
+            status: "open"
+        },
+    ]
+}
+
+export const CaseHistory = [
+  {
+    id: 1,
+    name: "Hypertension (HT)",
+    for: "hypertenstion"
+  },
+  {
+    id: 2,
+    name: "Smoking",
+    for: "smoking"
+  },
+  {
+    id: 3,
+    name: "Diabetes (DB)",
+    for: "diabetes"
+  },
+  {
+    id: 4,
+    name: "Renal Disease",
+    for: "renal-disease"
+  },
+  {
+    id: 5,
+    name: "Alcohol",
+    for: "alcohol"
+  },
+  {
+    id: 6,
+    name: "Asthma",
+    for: "asthma"
+  },
+  {
+    id: 7,
+    name: "Heart Disease",
+    for: "heart-disease"
+  },
+  {
+    id: 8,
+    name: "COPD",
+    for: "copd"
+  },
+]
