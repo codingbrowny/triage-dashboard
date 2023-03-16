27608edd0bd184c245a8ecb7ce01c58d1fd17420
codingbrowny <codingbrowny@gmail.com>
2023-03-16 05:36:34 -0700

    exports for components

---
+++ src/core/components/index.ts
@@ -0,0 +1,16 @@
+export { default as Sidebar } from "./sidebar";
+export { default as Spinner } from "./spinner";
+export { default as Navbar } from "./navbar";
+export { default as User } from "./user";
+export { default as Statistics } from "./statistics";
+export { default as StatHeader } from "./stats-header";
+export { default as AreaChart } from "./area-chart";
+export { default as PageTitle } from "./page-title";
+export { default as DataTable } from "./data-table";
+export { default as TableActionMenu } from "./table-action-menu";
+export { default as TabPanel } from "./tab-panel";
+export { default as AppDialog } from "./dialog";
+export { default as DeleteDoctor } from "./delete-doctor";
+export { default as RenewSubscription} from "./renew-subscription"
+export { default as AddCaseModal} from "./add-case"
+export { default as ViewCaseModal} from "./view-case"
