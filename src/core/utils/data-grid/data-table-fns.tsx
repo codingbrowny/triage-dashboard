144557fbf23fe63de3bb68a82fd62de3c0532490
codingbrowny <codingbrowny@gmail.com>
2023-03-16 00:40:49 -0700

    Refactored table cell render function

--- src/core/utils/data-grid/data-table-fns.tsx
+++ src/core/utils/data-grid/data-table-fns.tsx
@@ -4,42 +4,93 @@ import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 
 export type ActionType = {
   /**
-     * Callback fired when edit button is clicked
-     */
-  onEditData?: Function,
+   * Callback fired when edit button is clicked
+   */
+  onEditData?: Function;
   /**
    * Callback fired when renew subscription button is clicked
    */
-  onRenewSubscription?: Function,
+  onRenewSubscription?: Function;
   /**
    * Callback fired when delete button is clicked
    */
-  onDeleteData?: Function
-}
+  onDeleteData?: Function;
+};
 
 /** A function that handles how each cell in the data table is rendered.
  * It returns a modified copy of the actual data from the server
  */
- interface RenderCellInterface {
-  item: GridColDef,
-   actions: ActionType
- }
+interface RenderCellInterface {
+  item: GridColDef;
+  actions: ActionType;
+}
 
-export default function RenderDataCells({ item, actions }: RenderCellInterface) {
-  const {onDeleteData, onEditData, onRenewSubscription} = actions
+export default function RenderDataCells({
+  item,
+  actions,
+}: RenderCellInterface) {
+  const { onDeleteData, onEditData, onRenewSubscription } = actions;
   item = {
     ...item,
     renderCell(params: GridRenderCellParams) {
-      return <Typography className="text-gray-700">{params.value}</Typography>
-    }
-  }
-  if(item.field === "subscription_type") {
+      return <Typography className="text-gray-700">{params.value}</Typography>;
+    },
+  };
+  if (item.field === "subscription_type") {
     item = {
-      ...item, 
-      renderCell(params:GridRenderCellParams) {
-        return <Typography className="text-green-500">{params.value}</Typography>
-      }
-    }
+      ...item,
+      renderCell(params: GridRenderCellParams) {
+        return (
+          <Typography className="text-green-500">{params.value}</Typography>
+        );
+      },
+    };
+  }
+  if (item.field === "comments") {
+    item = {
+      ...item,
+      renderCell(params: GridRenderCellParams) {
+        return (
+          <Typography className="text-center w-full text-ellipsis overflow-hidden">
+            {params.value}
+          </Typography>
+        );
+      },
+    };
+  }
+  if (item.field === "subscription") {
+    item = {
+      ...item,
+      renderCell(params: GridRenderCellParams) {
+        let style = ""
+        if(params.value < 9) {
+          style = "text-red-500"
+        }else if(params.value < 20) {
+          style = "text-app-yellow"
+        }else { style = "text-green-600"}
+        return (
+          <Typography className={`w-full ${style}`}>
+            {params.value ===0 ? "no subscription" : params.value + " days"}
+          </Typography>
+        );
+      },
+    };
+  }
+  if (item.field === "status") {
+    item = {
+      ...item,
+      renderCell(params: GridRenderCellParams) {
+        return params.row.status === "open" ? (
+          <Typography className="text-center w-full text-green-500">
+            {params.value}
+          </Typography>
+        ) : (
+          <Typography className="text-center w-full text-red-500">
+            {params.value}
+          </Typography>
+        );
+      },
+    };
   }
 
   if (item.field === "action") {
@@ -49,13 +100,13 @@ export default function RenderDataCells({ item, actions }: RenderCellInterface) 
       renderCell(params: GridRenderCellParams) {
         return (
           <TableActionMenu
-          onEdit={onEditData}
-          onDelete={onDeleteData}
-          onRenew={onRenewSubscription}
+            onEdit={onEditData}
+            onDelete={onDeleteData}
+            onRenew={onRenewSubscription}
           />
         );
       },
     };
   }
-  return item
+  return item;
 }
