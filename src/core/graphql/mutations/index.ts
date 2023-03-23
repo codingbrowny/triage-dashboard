f9285da8a6525e1ec986d97b6943887e59f60577
codingbrowny <codingbrowny@gmail.com>
2023-03-23 16:57:05 +0000

    delete case mutation

--- src/core/graphql/mutations/index.ts
+++ src/core/graphql/mutations/index.ts
@@ -1,10 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const DeleteDoctor = gql`
-    mutation DeleteDoctor($id: String) {
+    mutation deleteDoctor($id: String) {
         deleteDoctor(id: $id) {
             id
             username
+        }
+    }
+`
+export const Deletecase = gql`
+    mutation deleteCase($id: String!) {
+        deleteCase(id: $id) {
+            message
         }
     }
 `
