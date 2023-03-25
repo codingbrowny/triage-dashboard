2d9d6fd46d47e8f61e9ed19b5ff24ffcc04f81eb
codingbrowny <codingbrowny@gmail.com>
2023-03-25 20:22:36 +0000

    snackbar component

---
+++ src/core/components/snackbar/index.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
+type SnackbarType = {
+  open: () => boolean | boolean;
+  message?: React.ReactNode;
+};
+
+export default function MySnackbar({ open, message }: SnackbarType) {
+  const [autoClose, setAutoClose] = React.useState<boolean>(open);
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setAutoClose(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      <Snackbar
+        open={autoClose}
+        autoHideDuration={3000}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        onClose={handleClose}
+        message={message}
+        action={action}
+      />
+    </div>
+  );
+}
