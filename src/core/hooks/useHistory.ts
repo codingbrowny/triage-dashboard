e96151044a6169a7f040735f9149e05301088cc7
codingbrowny <codingbrowny@gmail.com>
2023-03-25 09:55:22 +0000

    cases history selection hook'

--- src/core/hooks/useHistory.ts
+++ src/core/hooks/useHistory.ts
@@ -1,28 +1,27 @@
 import { ChangeEventHandler, useState } from "react";
 
 export const useHistory = () => {
-    const [history, setHistory] = useState<string[]>([]);
+  const [history, setHistory] = useState<string[]>([]);
 
-    const handleHistoryChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-
-      // Add history if not exist
-      if (e.target.checked) {
-        setHistory((prev) => {
-          if (prev.indexOf(e.target.value) < 0) {
-            return [...prev, e.target.value];
-          }
-          return prev;
-        });
-      } else {
-          setHistory((prev) => {
-            prev.splice(prev.indexOf(e.target.value), 1);
-            return prev;
-          });
-      }
-    };
-
-    return {
-        history,
-        handleHistoryChange
+  const handleHistoryChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    // Add history if not exist
+    if (e.target.checked) {
+      setHistory((prev) => {
+        if (prev.indexOf(e.target.value) < 0) {
+          return [...prev, e.target.value];
+        }
+        return prev;
+      });
+    } else {
+      setHistory((prev) => {
+        prev.splice(prev.indexOf(e.target.value), 1);
+        return prev;
+      });
     }
+  };
+
+  return {
+    history,
+    handleHistoryChange,
+  };
 };
