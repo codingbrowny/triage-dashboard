af7fe554b402bdcfc9a8b16b23725795420b6d79
codingbrowny <codingbrowny@gmail.com>
2023-03-30 01:27:44 +0000

    table footer props types

--- src/core/utils/data-grid/interface.ts
+++ src/core/utils/data-grid/interface.ts
@@ -15,6 +15,18 @@ export type ActionType = {
    * Callback fired when delete button is clicked
    */
   onDeleteData?: Function;
+  /**
+   * Callback fired when a row is clicked.
+   * Not called if the target clicked is an interactive element added by the built-in columns.
+   */
+  onRowClick?: GridEventListener<"rowClick">;
+};
+
+type FooterProps = {
+  /**
+   * Callback fired when footer refresh button is clicked
+   */
+  onRefresh?: Function;
 };
 
 export interface DataGridInterface {
@@ -23,32 +35,19 @@ export interface DataGridInterface {
   /** The table header */
   tableHeader: GridColDef[];
   /**
-   * If specified, the reviews section of the table is rendered with the review number.
-   * **ONLY** use for table with reviews section.
-   * */
-  hasReviewCount?: boolean;
-  /**
    * Determins the number of data to show per table page
    * default **10**
    */
   pageSize?: number;
-  /**
-   * Indicates whether review message is shown or not.
-   * **ONLY** use for table with reviews section.
-   */
-  hasReviewMessage?: boolean;
   props?: any;
   /**
    * The actions to perform on the cell data
    */
   actions: ActionType;
   /**
-   * Callback fired when a row is clicked.
-   * Not called if the target clicked is an interactive element added by the built-in columns.
-   */
-  onRowClick?: GridEventListener<"rowClick">;
-  /**
    * Indicated the loading state of the table
    */
   loading?: boolean;
+  footerProps?: FooterProps
+  
 }
