f64d97dcc2bc817267ca265d816ef12860d57b35
codingbrowny <codingbrowny@gmail.com>
2023-03-30 01:57:42 +0000

    disabled reload when loading

--- src/core/components/data-table/index.tsx
+++ src/core/components/data-table/index.tsx
@@ -19,12 +19,20 @@ const DataTable = ({
   tableHeader,
   data,
   actions,
-  onRowClick,
   pageSize = 10,
   loading = false,
+  footerProps,
 }: DataGridInterface) => {
   const apiRef = useGridApiRef();
   const [reload, setReload] = React.useState(false);
+
+  // change loading and run onRefresh callback
+  const handleRefresh = () => {
+    setReload(true);
+    if (footerProps?.onRefresh) {
+      footerProps.onRefresh();
+    }
+  };
 
   const EmptyDataOverlay = () => (
     <div className="flex flex-col justify-center items-center h-full">
@@ -42,14 +50,19 @@ const DataTable = ({
     return (
       <GridFooterContainer className="bg-slate-300/25">
         {/* Add what you want here */}
-        <LoadingButton
-          title={reload ? "Reloading" : "Refresh"}
-          loading={reload}
-          startIcon={<RefreshOutlinedIcon />}
-          className="capitalize mx-4 text-sm"
-          onClick={() => setReload(true)}
-        />
+        {footerProps?.onRefresh && (
+          <LoadingButton
+            title={
+              reload && loading ? "Reloading" : loading ? "Loading" : "Refresh"
+            }
+            loading={reload || loading}
+            startIcon={<RefreshOutlinedIcon />}
+            className="capitalize mx-4 text-sm"
+            onClick={() => handleRefresh()}
+          />
+        )}
         <GridFooter
+          className="ml-auto"
           sx={{
             border: "none", // To delete double border.
           }}
@@ -57,6 +70,10 @@ const DataTable = ({
       </GridFooterContainer>
     );
   }
+
+  React.useEffect(() => {
+    !loading && setReload(false);
+  }, [loading]);
 
   const DefaultProgress = () => {
     return (
@@ -78,7 +95,7 @@ const DataTable = ({
         checkboxSelection
         disableRowSelectionOnClick
         loading={loading}
-        onRowClick={onRowClick}
+        onRowClick={actions.onRowClick}
         pagination
         slots={{
           pagination: TablePagination,
