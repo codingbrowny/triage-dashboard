d8e845c24b5507585d3e498346ed7dcec9817e24
codingbrowny <codingbrowny@gmail.com>
2023-03-31 00:58:51 +0000

    doctors and case count

--- src/core/components/stats-header/index.tsx
+++ src/core/components/stats-header/index.tsx
@@ -5,11 +5,11 @@ import { AllDoctors } from "@/core/graphql/queries/doctors";
 import { useQuery } from "@apollo/client";
 
 const StatHeader = () => {
-  const cases = useQuery(AllCases);
-  const doctors = useQuery(AllDoctors);
+  const cases = useQuery(AllCases, { pollInterval: 5000, notifyOnNetworkStatusChange:true });
+  const doctors = useQuery(AllDoctors, { pollInterval: 5000, notifyOnNetworkStatusChange:true });
 
   //No. of cases
-  const allCase = cases.data?.cases.length
+  const allCase = cases.data?.cases.length;
   //No. of pending cases
   const pendingCases = cases.data?.cases.filter(
     (item: any) => item.claimed === false
@@ -23,29 +23,26 @@ const StatHeader = () => {
     (doc: any) => doc.isPhysician === false
   ).length;
 
-  console.log(allCase);
-  
-
   return (
     <div className="flex justify-between items-center gap-5 md:gap-6 flex-wrap md:flex-nowrap px-2 md:px-0">
       <Statistics
         title="Cases"
-        value={!cases.loading?allCase: "..."}
+        value={!cases.loading ? allCase : "..."}
         color={"from-primary/50 to-primary"}
       />
       <Statistics
         title={"Pending Cases"}
-        value={!cases.loading?pendingCases: "..."}
+        value={!cases.loading ? pendingCases : "..."}
         color={"from-app-yellow/50 to-app-yellow"}
       />
       <Statistics
         title={"Doctors"}
-        value={!doctors.loading?onlyDoctors: "..."}
+        value={!doctors.loading ? onlyDoctors : "..."}
         color={"from-app-blue/50 to-app-blue"}
       />
       <Statistics
         title={"Consultants"}
-        value={!doctors.loading?consultants: "..."}
+        value={!doctors.loading ? consultants : "..."}
         color={"from-app-red/50 to-app-red"}
       />
     </div>
